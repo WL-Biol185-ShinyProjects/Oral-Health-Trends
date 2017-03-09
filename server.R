5bc099f05a572a5cbb548c291ae4dd185cc29699
Betsy <hensonb17@mail.wlu.edu>
2017-03-09 13:03:14 -0500

    saved files

--- server.R
+++ server.R
@@ -1,5 +1,17 @@
 library(shiny)
 
-
-states <- geojsonio::geojson_read("json/us-states.geojson", what = "sp")
-class(states)
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  output$distPlot <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+  })
+  
+})
