ccb51253987fde2862a804be4be1799a175d38ca
Betsy <hensonb17@mail.wlu.edu>
2017-03-16 13:11:26 -0400

    fixed data for heat map. map is still not working tho

--- heatmap.R
+++ heatmap.R
@@ -1,10 +1,18 @@
 library(tidyverse)
 library(leaflet)
+library(RColorBrewer)
+
 
 states <- rgdal::readOGR("statedata.json", "OGRGeoJSON")
+bins <- c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+pal <- colorBin("Spectral", domain = newtable$Data_Value, bins = bins)
+newtable.data_value <- filter(newtable, Break_Out_Category == "None", Response == "Yes", Indicator == "Adults aged 18+ who have visited a dentist or dental clinic in the past year")
+read.table(newtable.data_value)
 
 leaflet(states) %>% setView(lng=-100, lat= 40, zoom=4) %>%
-  addTiles() %>%
-  addPolygons()
+  addTiles() 
+  addPolygons(
+  fillColor = ~pal(newtable.data_value$Data_Value)
+  )
     
 
