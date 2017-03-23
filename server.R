d2853877cbc92fd91d7c340af4d2c766245b6bee
Kathryn <rileyk17@mail.wlu.edu>
2017-03-23 12:31:30 -0400

    update

--- server.R
+++ server.R
@@ -5,7 +5,6 @@ function(input, output) {
   output$genderplot <- renderPlot({
       gender %>%
         filter(LocationDesc == input$LocationDesc) %>%
-        ggplot(aes(Break_Out, Data_Value)) + xlab("Gender") + ylab("Percent") + geom_bar(stat = "identity")  
+        ggplot(aes(Break_Out, Data_Value)) + xlab("Gender") + ylab("Percent") + geom_bar(stat = "identity")})  
 
-  })
 }
