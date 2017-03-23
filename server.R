f2e54d12f51dfe9e5be2704512640f98e5850a5d
Kathryn <rileyk17@mail.wlu.edu>
2017-03-23 12:17:34 -0400

    changed axes labels

--- server.R
+++ server.R
@@ -5,7 +5,7 @@ function(input, output) {
   output$genderplot <- renderPlot({
       gender %>%
         filter(LocationDesc == input$LocationDesc) %>%
-        ggplot(aes(Break_Out, Data_Value)) + geom_bar(stat = "identity")  
+        ggplot(aes(Break_Out, Data_Value)) + xlab("Gender") + ylab("Percent") + geom_bar(stat = "identity")  
 
   })
 }
