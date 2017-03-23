3002955db54cb80dc725b2644500896a39c2ad0e
Betsy <hensonb17@mail.wlu.edu>
2017-03-23 12:33:20 -0400

    MAP DONE!!

--- heatmap.R
+++ heatmap.R
@@ -1,17 +1,19 @@
 library(tidyverse)
 library(leaflet)
 library(RColorBrewer)
-
+library(htmltools)
 
 states <- rgdal::readOGR("statedata.json", "OGRGeoJSON")
 bins <- c(0, 50, 55, 60, 65, 70, 75, 80, 100)
 pal <- colorBin("YlGnBu", domain = newtable.data_value$Data_Value, bins = bins)
 newtable.data_value <- filter(newtable, Break_Out_Category == "None", Response == "Yes", Indicator == "Adults aged 18+ who have visited a dentist or dental clinic in the past year")
 
- labels <- sprintf(
-   "People that visit the dentist",
-  states$name, newtable.data_value$Data_Value
-)
+
+
+labels <- sprintf(
+  "<strong>%s</strong><br/>%g Percent of People",
+  newtable.data_value$LocationDesc, newtable.data_value$Data_Value
+) %>% lapply(htmltools::HTML)
 
 leaflet(data = states) %>% setView(lng=-100, lat= 40, zoom=4) %>%
   addTiles() %>%
@@ -25,11 +27,11 @@ leaflet(data = states) %>% setView(lng=-100, lat= 40, zoom=4) %>%
     dashArray = "",
     fillOpacity = 0.7,
     bringToFront = TRUE),
-  label = labels,labels,
+  label = labels,
   labelOptions = labelOptions(
-     style= list("font-weight" = "normal", padding = "3px 8px"),
-     textsize = "15px",
-     direction = "auto"
+      style= list("font-weight" = "normal", padding = "3px 8px"),
+      textsize = "15px",
+    direction = "auto"
    )) %>%
   addLegend(pal = pal, values= ~newtable.data_value$Data_Value, opacity = 0.7, title= NULL, position= "bottomright"
   )
