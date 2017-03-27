72b482ccdd0557f7ecdd4d727eefdff42245eaa2
Betsy <hensonb17@mail.wlu.edu>
2017-03-27 14:27:02 -0400

    now the map is done

--- heatmap.R
+++ heatmap.R
@@ -3,23 +3,23 @@ library(leaflet)
 library(RColorBrewer)
 library(htmltools)
 
-colname(newtable.data_value)[colnames(newtable.data_value)== "LocationDesc"] <- NAME
-View(newtable.data_value)
 states <- rgdal::readOGR("statedata.json", "OGRGeoJSON")
 bins <- c(0, 50, 55, 60, 65, 70, 75, 80, 100)
 pal <- colorBin("YlGnBu", domain = newtable.data_value$Data_Value, bins = bins)
 newtable.data_value <- filter(newtable, Break_Out_Category == "None", Response == "Yes", Indicator == "Adults aged 18+ who have visited a dentist or dental clinic in the past year")
 
+joinedTable <- left_join(states@data, filter(newtable.data_value, Year == 2014), c("NAME" = "LocationDesc"))
+states@data <- joinedTable
 
 labels <- sprintf(
   "<strong>%s</strong><br/>%g Percent of People",
-  newtable.data_value$LocationDesc, newtable.data_value$Data_Value
+  joinedTable$NAME, joinedTable$Data_Value
 ) %>% lapply(htmltools::HTML)
 
-leaflet(data = states) %>% setView(lng=-100, lat= 40, zoom=4) %>%
+leaflet(states)  %>% setView(lng = -100, lat = 40, zoom = 4) %>%
   addTiles() %>%
   addPolygons(
-  fillColor = ~pal(newtable.data_value$Data_Value), 
+  fillColor = ~pal2(Data_Value), 
   weight = 2,
   color = "white",
   highlight= highlightOptions(
@@ -28,13 +28,29 @@ leaflet(data = states) %>% setView(lng=-100, lat= 40, zoom=4) %>%
     dashArray = "",
     fillOpacity = 0.7,
     bringToFront = TRUE),
-  label = labels,
+label = labels,
   labelOptions = labelOptions(
-      style= list("font-weight" = "normal", padding = "3px 8px"),
-      textsize = "15px",
-    direction = "auto"
+  style= list("font-weight" = "normal", padding = "3px 8px"),
+  textsize = "15px",
+  direction = "auto"
    )) %>%
-  addLegend(pal = pal, values= ~newtable.data_value$Data_Value, opacity = 0.7, title= NULL, position= "bottomright"
+  addLegend(pal = pal, values= ~Data_Value, opacity = 0.7, title= NULL, position= "bottomright"
   )
+
+
+
+
+# THIS WORKS: 
+pal2 <- colorBin("YlGnBu", domain = states@data$Data_Value)
+
+leaflet(states) %>%
+  addTiles() %>%
+  addPolygons(
+    label = ~NAME,
+    fillColor = ~pal2(Data_Value)
+      )
+  
+
+# %>% setView(lng=-100, lat= 40, zoom=4)
 
 
