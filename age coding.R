ea4d62f6bd2af16ad4ed24c09b5fe9fb2527cfcc
Betsy <hensonb17@mail.wlu.edu>
2017-03-29 12:27:42 -0400

    age graph

--- age coding.R
+++ age coding.R
@@ -4,19 +4,14 @@ library(dplyr)
 
 age5 <- filter(dental_data_RAW, Break_Out_Category == "Age", LocationDesc != "Guam", LocationDesc != "Puerto Rico", Response == "Yes", Year == "2014")
 
-inputPanel(
-  selectInput("LocationDesc", label = "State", choices = unique(age5$LocationDesc))
-)
+ggplot(age5, aes(Break_Out, Data_Value)) + xlab("Age Group") + ylab("Percent") + geom_bar(stat = "identity")
 
-renderPlot({
+inputPanel(
+  selectInput("LocationDesc", label = "State", choices = unique(age5$LocationDesc)),
+  plotOutput("age5plot"))
+
+output$age5plot <- renderPlot({
   age5 %>%
     filter(LocationDesc == input$LocationDesc) %>%
-    ggplot(aes(Break_Out, Data_Value)) + geom_bar(stat = "identity")
-})
+    ggplot(aes(Break_Out, Data_Value)) + xlab("Age") + ylab("Percent") + geom_bar(stat = "identity")})
 
-renderPlot({
-  age5 %>%
-    ggplot(aes(LocationAbbr, Data_Value)) + geom_bar(stat = "identity")
-})
-
-ggplot(age5, aes(LocationAbbr, Data_Value) + xlab("State") + ylab("Percent")) + geom_bar(stat = "identity")
