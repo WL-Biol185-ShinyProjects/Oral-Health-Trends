870565eab1d6cd2100e213f3407d27a42cbf9a8c
Kathryn <rileyk17@mail.wlu.edu>
2017-03-30 12:30:29 -0400

    slanted axes

--- server.R
+++ server.R
@@ -20,15 +20,15 @@ function(input, output) {
   output$raceplot <- renderPlot({
      race %>%
         filter(LocationDesc == input$LocationDesc3) %>%
-        ggplot(aes(Break_Out, Data_Value)) + xlab("Race") + ylab("Percent") + geom_bar(stat = "identity")})
+        ggplot(aes(Break_Out, Data_Value)) + xlab("Race") + ylab("Percent") + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = 60, hjust = 1))})
 
   output$statewideblack <- renderPlot({
       raceB %>%
-        ggplot(aes(LocationDesc, Black)) + xlab("State") + ylab("Percent") + geom_bar(stat = "identity")})
+        ggplot(aes(LocationAbbr, Black)) + xlab("State") + ylab("Percent") + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = 60, hjust = 1))})
   
   output$incomeplot <- renderPlot({
       income %>%
         filter(LocationDesc == input$LocationDesc4) %>%
-        ggplot(aes(Break_Out, Data_Value)) + xlab("Income") + ylab("Percent") + geom_bar(stat = "identity")})
+        ggplot(aes(Break_Out, Data_Value)) + xlab("Income") + ylab("Percent") + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = 60, hjust = 1))})
   
   }
