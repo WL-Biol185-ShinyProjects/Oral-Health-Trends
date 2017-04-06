538b490b3fe7a1b86f8b659309d596dd3effe97c
Betsy <hensonb17@mail.wlu.edu>
2017-04-06 12:15:53 -0400

    working RDS

--- server.R
+++ server.R
@@ -80,7 +80,7 @@ function(input, output) {
     countryplot%>%
       ggplot(aes(X2012,X2014, color=LocationDesc)) + xlab("2012_values") + ylab("2014_values") + geom_point() + theme(legend.position = "bottom") + theme(legend.text = element_text(size=9))})
   
-  map <- readRDS(file = "map.rds")
+  map <- readRDS(file = "heatmap.rds")
     output$map <- renderLeaflet({
         leaflet(states)  %>% setView(lng = -100, lat = 40, zoom = 4) %>%
         addTiles() %>%
